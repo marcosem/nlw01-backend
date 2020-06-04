2b351d7e3251a87113f6cf5f939f2f5c6c6df40c
Marcos E Mathias <marcos-github@memathias.com>
2020-06-03 23:27:37 -0300

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import { resolve } from 'path';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')))
+
+app.listen(3333);
